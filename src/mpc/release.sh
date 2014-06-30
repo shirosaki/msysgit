59efcff13d4104270007fd68c5274b9ddd2bb1d9
Hiroshi Shirosaki <h.shirosaki@gmail.com>
2014-06-30 22:36:45 +0900

    Add mpc 1.0.2

---
+++ src/mpc/release.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+cd "$(dirname "$0")" && . ../common/update-lib.sh
+
+check_pristine
+
+package=mpc
+version=1.0.2
+url=http://www.multiprecision.org/mpc/download/
+d=$package-$version
+tar=$d.tar.gz
+
+opt_flags="-O3 -march=i386"
+export CFLAGS=${CFLAGS:-"${opt_flags}"}
+export CPPFLAGS="${CPPFLAGS} -D__CYGWIN__"
+export LDFLAGS="${LDFLAGS} -s -Wl,--enable-auto-import"
+# search gmp library
+export PATH=/usr/lib:$PATH
+
+configure_options="--prefix=/usr \
+	--enable-shared \
+	--disable-static"
+
+download &&
+extract &&
+apply_patches
+
+setup
+compile
+
+# update index
+FILELIST=fileList.txt
+
+pre_install
+
+(cd "$d" && make install) || die "Could not install"
+
+post_install
