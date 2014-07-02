5a39150a318e9df9e4ca28833f2a019fa5aea50a
Hiroshi Shirosaki <h.shirosaki@gmail.com>
2014-07-02 11:09:03 +0900

    Add automake 1.14

---
+++ src/automake1.14/release.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+cd "$(dirname "$0")" && . ../common/update-lib.sh
+
+check_pristine
+
+package=automake
+version=1.14
+url=ftp://ftp.gnu.org/pub/gnu/$package/
+d=$package-$version
+tar=$d.tar.gz
+
+opt_flags="-O3 -s -march=i386"
+export CFLAGS=${CFLAGS:-"${opt_flags}"}
+export CPPFLAGS="${CPPFLAGS} -D__CYGWIN__"
+export LDFLAGS="${LDFLAGS} -Wl,--enable-auto-import"
+
+configure_options="--prefix=/usr"
+
+download &&
+extract &&
+apply_patches
+
+setup
+compile
+
+# update index
+FILELIST=fileList.txt
+
+pre_install
+
+(cd "$d" && make install) || die "Could not install"
+
+post_install
